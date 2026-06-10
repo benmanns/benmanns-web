7840d9a701cfaa33033fef7fa35f86208164ee60
Benjamin Manns <benmanns@gmail.com>
2026-06-10 13:35:30 -0400

    Update devenv

--- devenv.nix
+++ devenv.nix
@@ -9,21 +9,36 @@
 {
   dotenv.disableHint = true;
 
-  packages = [ pkgs.git ];
+  packages = with pkgs; [
+    git
+  ];
 
   enterTest = ''
     git --version
   '';
 
-  languages.javascript.enable = true;
-  languages.javascript.npm.enable = true;
-  languages.javascript.yarn.enable = true;
-  languages.javascript.yarn.install.enable = true;
-  languages.nix.enable = true;
-  languages.typescript.enable = true;
+  languages = {
+    javascript = {
+      enable = true;
+      npm.enable = true;
+      yarn.enable = true;
+      yarn.install.enable = true;
+    };
+    nix.enable = true;
+    typescript.enable = true;
+  };
 
-  pre-commit.hooks.eslint.enable = true;
-  pre-commit.hooks.eslint.settings.binPath = "node_modules/.bin/eslint";
-  pre-commit.hooks.nixfmt-rfc-style.enable = true;
-  pre-commit.hooks.prettier.enable = true;
+  git-hooks.hooks.eslint.enable = true;
+  git-hooks.hooks.eslint.settings.binPath = "node_modules/.bin/eslint";
+  git-hooks.hooks.treefmt.enable = true;
+
+  treefmt = {
+    enable = true;
+    config = {
+      programs = {
+        nixfmt.enable = true;
+        prettier.enable = true;
+      };
+    };
+  };
 }
